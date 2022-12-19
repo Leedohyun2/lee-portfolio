a8f3ed98b9d955af8cfe6cfadacf8c18c03c8f0a
Leedohyun <ehqhddl123@naver.com>
2022-12-19 10:49:23 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// navbar 스크롤 시 변화주기
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(`navabarHeight: ${navbarHeight}`);
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// navbar에서 버튼 클릭 시 지정된 위치로 이동
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+});
